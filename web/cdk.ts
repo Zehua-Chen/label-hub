80b85a8101f91d01827a00468c6f05a46aedab05
Zehua-Chen <zehua-chen@outlook.com>
2022-11-01 14:50:58 -0400

    [feat] Improve Bucket Name

--- web/cdk.ts
+++ web/cdk.ts
@@ -1,23 +1,22 @@
-import * as cdk from 'aws-cdk-lib';
-import * as path from 'path';
+import { RemovalPolicy, aws_s3 as s3, aws_iam as iam } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class Web extends Construct {
   constructor(scope: Construct, id: string, props: unknown) {
     super(scope, id);
 
-    const bucket = new cdk.aws_s3.Bucket(this, 'LabelHubHosting', {
-      bucketName: 'lable-hub-hosting',
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    const bucket = new s3.Bucket(this, 'FrontendHosting', {
+      bucketName: 'coms6998-fa22-lable-hub-hosting',
+      removalPolicy: RemovalPolicy.DESTROY,
       websiteIndexDocument: 'index.html',
       websiteErrorDocument: '404.html',
     });
 
     bucket.addToResourcePolicy(
-      new cdk.aws_iam.PolicyStatement({
-        effect: cdk.aws_iam.Effect.ALLOW,
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
         actions: ['s3:GetObject'],
-        principals: [new cdk.aws_iam.AnyPrincipal()],
+        principals: [new iam.AnyPrincipal()],
       })
     );
   }
