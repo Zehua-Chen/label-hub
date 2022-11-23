3975b5d2dd100798dc9b0200bcac8170b771085a
Zehua-Chen <zehua-chen@outlook.com>
2022-11-23 16:52:54 -0500

    [test] Setup Component Test

--- web/jest.config.js
+++ web/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(jsx*|tsx*)?$': `<rootDir>/jest-preprocess.js`,
   },
---
+++ web/src/components/ProtectedRoute/index.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ProtectedRoute from '.';
+
+describe('ProtectedRoute', () => {
+  it('render', () => {
+    const navigate = jest.fn();
+
+    render(
+      <ProtectedRoute condition={() => true} navigate={navigate}>
+        <div>Content</div>
+      </ProtectedRoute>
+    );
+
+    expect(navigate).not.toHaveBeenCalled();
+    expect(screen.getByText('Content')).toBeTruthy();
+  });
+
+  it('fallback', () => {
+    const navigate = jest.fn();
+
+    render(
+      <ProtectedRoute
+        condition={() => false}
+        navigate={navigate}
+        fallback="/fallback"
+      >
+        <div>Content</div>
+      </ProtectedRoute>
+    );
+
+    expect(navigate).toHaveBeenCalledWith('/fallback');
+    expect(() => screen.getByText('Content')).toThrow();
+  });
+});
