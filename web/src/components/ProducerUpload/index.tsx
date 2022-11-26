78da2be73de34f16ca37d741bbc861d0b0ee9a44
Zehua-Chen <zehua-chen@outlook.com>
2022-11-26 14:51:15 -0500

    [feat] Navigate Back on Save

--- web/src/components/ProducerUpload/index.tsx
+++ web/src/components/ProducerUpload/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { navigate } from 'gatsby';
 import Layout from 'src/components/Layout';
 import Navbar from 'src/components/Navbar';
 import UploadButton from 'src/components/UploadButton';
@@ -6,6 +7,7 @@ import UploadButton from 'src/components/UploadButton';
 function Upload(): JSX.Element {
   const [image, setImage] = useState<File | null>(null);
   const [imageURL, setImageURL] = useState('');
+  const [tags, setTags] = useState([] as string[]);
 
   useEffect(() => {
     if (!image) {
@@ -15,11 +17,18 @@ function Upload(): JSX.Element {
     const url = URL.createObjectURL(image);
     setImageURL(url);
 
+    // TODO: use tags from backend for production
+    setTags(['tag1', 'tag2']);
+
     return () => URL.revokeObjectURL(url);
   }, [image]);
 
   function onUploadChange(file: File) {
     setImage(file);
+  }
+
+  function onSaveClick() {
+    navigate('/app/producer/');
   }
 
   return (
@@ -29,14 +38,24 @@ function Upload(): JSX.Element {
           <div className='col-8'>
             <img className='img-fluid' src={imageURL} />
           </div>
-          <div className='col'>Tags</div>
+          <div className='col'>
+            <ul>
+              {tags.map((tag, index) => (
+                <li key={index}>{tag}</li>
+              ))}
+            </ul>
+          </div>
         </div>
         <div className='row mt-2'>
           <div className='col-8 d-grid'>
             {!image ? (
               <UploadButton onChange={onUploadChange}>Upload</UploadButton>
             ) : null}
-            {image ? <button className='btn btn-primary'>Save</button> : null}
+            {image ? (
+              <button className='btn btn-primary' onClick={onSaveClick}>
+                Save
+              </button>
+            ) : null}
           </div>
         </div>
       </form>
