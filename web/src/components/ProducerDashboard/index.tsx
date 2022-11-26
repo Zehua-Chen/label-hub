af3b2a053a6454a2306b9115606d1e4ba00a256b
Zehua-Chen <zehua-chen@outlook.com>
2022-11-26 14:44:39 -0500

    [feat] Create Upload Interface

--- web/src/components/ProducerDashboard/index.tsx
+++ web/src/components/ProducerDashboard/index.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import DashboardLayout from 'src/components/DashboardLayout';
-import { useApi } from 'src/services/api/utils';
 
 function ProducerDashboard() {
-  const api = useApi();
-
-  async function test() {
-    await api.photosGet();
-    console.log('response received');
-  }
-
   return (
     <DashboardLayout mode='Producer' sidebar={<div>Sidebar</div>}>
       <Link className='btn btn-primary' to='/app/producer/upload'>
--- web/src/components/ProducerUpload/index.tsx
+++ web/src/components/ProducerUpload/index.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from 'src/components/Layout';
 import Navbar from 'src/components/Navbar';
+import UploadButton from 'src/components/UploadButton';
 
 function Upload(): JSX.Element {
+  const [image, setImage] = useState<File | null>(null);
+  const [imageURL, setImageURL] = useState('');
+
+  useEffect(() => {
+    if (!image) {
+      return;
+    }
+
+    const url = URL.createObjectURL(image);
+    setImageURL(url);
+
+    return () => URL.revokeObjectURL(url);
+  }, [image]);
+
+  function onUploadChange(file: File) {
+    setImage(file);
+  }
+
   return (
     <Layout navigation={<Navbar title='Upload'></Navbar>}>
-      <form></form>
+      <form className='container-fluid'>
+        <div className='row'>
+          <div className='col-8'>
+            <img className='img-fluid' src={imageURL} />
+          </div>
+          <div className='col'>Tags</div>
+        </div>
+        <div className='row mt-2'>
+          <div className='col-8 d-grid'>
+            {!image ? (
+              <UploadButton onChange={onUploadChange}>Upload</UploadButton>
+            ) : null}
+            {image ? <button className='btn btn-primary'>Save</button> : null}
+          </div>
+        </div>
+      </form>
     </Layout>
   );
 }
