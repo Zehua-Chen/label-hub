35f6f7766e2f8bf8913065ea424ff1dd282b4e0f
Zehua-Chen <zehua-chen@outlook.com>
2022-11-30 16:15:01 -0500

    [feat] Implement Tags Editor

--- web/src/components/ProducerUpload/index.test.tsx
+++ web/src/components/ProducerUpload/index.test.tsx
@@ -7,15 +7,15 @@ import { render, screen, act } from '@testing-library/react';
 import ProducerUpload from '.';
 import { getInputElement } from 'src/components/UploadButton/test-utils';
 
+Object.defineProperty(globalThis, 'URL', {
+  value: {
+    createObjectURL: jest.fn(() => ''),
+    revokeObjectURL: jest.fn(),
+  },
+});
+
 describe('Upload', () => {
   it('Save', async () => {
-    Object.defineProperty(globalThis, 'URL', {
-      value: {
-        createObjectURL: jest.fn(() => ''),
-        revokeObjectURL: jest.fn(),
-      },
-    });
-
     const user = userEvent.setup();
     const file = new File(['hello'], 'test.png');
 
@@ -29,5 +29,49 @@ describe('Upload', () => {
     });
 
     expect(navigate).toHaveBeenCalledWith('/app/producer/');
+  });
+});
+
+describe('Tags', () => {
+  it('Manage Tags', async () => {
+    const user = userEvent.setup();
+    const file = new File(['hello'], 'test.png');
+
+    render(<ProducerUpload />);
+
+    await act(async () => {
+      await user.upload(getInputElement(), file);
+    });
+
+    const newTag: HTMLInputElement = screen.getByTestId('newTag');
+    const addTag = screen.getByText('Add Tag');
+
+    await act(async () => {
+      await user.type(newTag, 'Tag 1');
+      await user.click(addTag);
+    });
+
+    let tag1s = screen.queryAllByText('Tag 1');
+
+    expect(newTag.value).toBe('');
+    expect(tag1s.length).toBe(1);
+
+    await act(async () => {
+      await user.type(newTag, 'Tag 2');
+      await user.click(addTag);
+    });
+
+    expect(newTag.value).toBe('');
+
+    await act(async () => {
+      const deletTag = screen.getByTestId('delete-Tag 1');
+      await user.click(deletTag);
+    });
+
+    tag1s = screen.queryAllByText('Tag 1');
+    const tag2s = screen.queryAllByText('Tag 2');
+
+    expect(tag1s.length).toBe(0);
+    expect(tag2s.length).toBe(1);
   });
 });
--- web/src/components/ProducerUpload/index.tsx
+++ web/src/components/ProducerUpload/index.tsx
@@ -1,13 +1,76 @@
-import React, { useState, useEffect, MouseEvent } from 'react';
+import React, { useState, useEffect, MouseEvent, useReducer } from 'react';
 import { navigate } from 'gatsby';
+import produce from 'immer';
 import Layout from 'src/components/Layout';
 import Navbar from 'src/components/Navbar';
 import UploadButton from 'src/components/UploadButton';
 
+interface TagsState {
+  readonly tags: readonly string[];
+  readonly newTag: string;
+}
+
+interface TagsActionSetTags {
+  type: 'setTags';
+  tags: string[];
+}
+
+interface TagsActionSetNewTag {
+  type: 'setNewTag';
+  newTag: string;
+}
+
+interface TagsActionAddNewTag {
+  type: 'addNewTag';
+}
+
+interface TagsActionDeleteTag {
+  type: 'deleteTag';
+  index: number;
+}
+
+type TagsAction =
+  | TagsActionSetTags
+  | TagsActionSetNewTag
+  | TagsActionAddNewTag
+  | TagsActionDeleteTag;
+
+function tagsReducer(state: TagsState, action: TagsAction): TagsState {
+  switch (action.type) {
+    case 'setTags': {
+      return produce(state, (draft) => {
+        draft.tags = action.tags;
+      });
+    }
+    case 'setNewTag': {
+      return produce(state, (draft) => {
+        draft.newTag = action.newTag;
+      });
+    }
+    case 'addNewTag': {
+      return produce(state, (draft) => {
+        draft.tags.push(draft.newTag);
+        draft.newTag = '';
+      });
+    }
+    case 'deleteTag': {
+      return produce(state, (draft) => {
+        draft.tags.splice(action.index, 1);
+      });
+    }
+    default:
+      return state;
+  }
+}
+
 function Upload(): JSX.Element {
   const [image, setImage] = useState<File | null>(null);
   const [imageURL, setImageURL] = useState('');
-  const [tags, setTags] = useState([] as string[]);
+
+  const [tagsState, tagsDispatch] = useReducer(tagsReducer, {
+    tags: [],
+    newTag: '',
+  });
 
   useEffect(() => {
     if (!image) {
@@ -18,7 +81,7 @@ function Upload(): JSX.Element {
     setImageURL(url);
 
     // TODO: use tags from backend for production
-    setTags(['tag1', 'tag2']);
+    tagsDispatch({ type: 'setTags', tags: ['tag1', 'tag2'] });
 
     return () => URL.revokeObjectURL(url);
   }, [image]);
@@ -32,6 +95,16 @@ function Upload(): JSX.Element {
     navigate('/app/producer/');
   }
 
+  function onTagDelete(e: MouseEvent, index: number) {
+    e.preventDefault();
+    tagsDispatch({ type: 'deleteTag', index });
+  }
+
+  function onAddTagClick(e: MouseEvent) {
+    e.preventDefault();
+    tagsDispatch({ type: 'addNewTag' });
+  }
+
   return (
     <Layout navigation={<Navbar title='Upload'></Navbar>}>
       <form className='container-fluid'>
@@ -41,10 +114,37 @@ function Upload(): JSX.Element {
           </div>
           <div className='col'>
             <ul>
-              {tags.map((tag, index) => (
-                <li key={index}>{tag}</li>
+              {tagsState.tags.map((tag, index) => (
+                <li key={index}>
+                  <span>{tag}</span>
+                  <button
+                    className='btn btn-primary'
+                    onClick={(e) => onTagDelete(e, index)}
+                    data-testid={`delete-${tag}`}
+                  >
+                    Delete
+                  </button>
+                </li>
               ))}
             </ul>
+            <div>
+              <label htmlFor='newTag' className='form-label'>
+                New Tag
+              </label>
+              <input
+                type='text'
+                className='form-control'
+                id='newTag'
+                data-testid='newTag'
+                value={tagsState.newTag}
+                onChange={(e) =>
+                  tagsDispatch({ type: 'setNewTag', newTag: e.target.value })
+                }
+              />
+            </div>
+            <button className='btn btn-primary' onClick={onAddTagClick}>
+              Add Tag
+            </button>
           </div>
         </div>
         <div className='row mt-2'>
