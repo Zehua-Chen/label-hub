69093b9c6cc608d64b4a1f4e0f1eb6d4e30a7a1a
Zehua-Chen <zehua-chen@outlook.com>
2022-12-08 12:33:39 -0500

    [chore] Use DEV_SSR flag

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -3,5 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 module.exports = {
+  flags: {
+    DEV_SSR: true,
+  },
   plugins: ['gatsby-plugin-pnpm', 'gatsby-plugin-root-import'],
 };
