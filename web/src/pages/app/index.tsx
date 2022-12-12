acd01622db8e06cda81bca89eb915c9a11fa81ba
Zehua-Chen <zehua-chen@outlook.com>
2022-12-11 19:52:30 -0500

    [feat] Protect /app

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -2,19 +2,23 @@ import * as React from 'react';
 import { Link } from 'gatsby';
 import HeadContent from 'src/components/HeadContent';
 import Layout from 'src/components/Layout';
+import ProtectedRoute from 'src/components/ProtectedRoute';
+import { authRouteGuard } from 'src/services/auth';
 
 function App() {
   return (
-    <Layout>
-      <ul className='list-group'>
-        <li className='list-group-item'>
-          <Link to='producer'>Producer</Link>
-        </li>
-        <li className='list-group-item'>
-          <Link to='consumer'>Consumer</Link>
-        </li>
-      </ul>
-    </Layout>
+    <ProtectedRoute condition={authRouteGuard()}>
+      <Layout>
+        <ul className='list-group'>
+          <li className='list-group-item'>
+            <Link to='producer'>Producer</Link>
+          </li>
+          <li className='list-group-item'>
+            <Link to='consumer'>Consumer</Link>
+          </li>
+        </ul>
+      </Layout>
+    </ProtectedRoute>
   );
 }
 
