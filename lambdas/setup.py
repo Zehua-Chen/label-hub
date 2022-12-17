a77e50c1aac82c95106c4814d61ffb6c823f541c
tsai00150 <tsai00150@outlook.com>
2022-12-17 15:46:45 -0500

    delete photos stack

--- lambdas/setup.py
+++ lambdas/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="label_hub",
     packages=find_packages(),
-    install_requires=["requests"],
+    install_requires=["requests", "requests-aws4auth", "opensearchpy"],
 )
