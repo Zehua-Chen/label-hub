6df017c8a8dd71678b92fd9585203aef88c970c1
Fatima2508 <93784775+Fatima2508@users.noreply.github.com>
2022-12-19 21:32:13 -0500

    user info inputs

--- lambdas/label_hub/lambdas/userinfo_get/lambda_function.py
+++ lambdas/label_hub/lambdas/userinfo_get/lambda_function.py
@@ -6,16 +6,17 @@ import json
 import boto3
 import os
 
+region = 'us-east-1'
 client = boto3.client('dynamodb')
 tableName = host = os.environ['dynamodb_tableName']
 
 
 def lambda_handler(event, context):
-    #get userid
-    # cog = boto3.client("cognito-idp", region_name=region)
-    # user_id = cog.get_user(AccessToken=idtoken)['Username']
+    body = json.loads(event['body'])
+    idtoken = body['idtoken']
+    cog = boto3.client("cognito-idp", region_name=region)
+    user_id = cog.get_user(AccessToken=idtoken)['Username']
 
-    user_id = 'asdf-hgd'
     try:
         data = client.get_item(TableName=tableName, Key={'id': {'S': user_id}})
         return {
--- lambdas/label_hub/lambdas/userinfo_put/lambda_function.py
+++ lambdas/label_hub/lambdas/userinfo_put/lambda_function.py
@@ -8,19 +8,21 @@ import os
 
 client = boto3.client('dynamodb')
 tableName = host = os.environ['dynamodb_tableName']
-
+region = 'us-east-1'
 
 def lambda_handler(event, context):
     #get userid
     # cog = boto3.client("cognito-idp", region_name=region)
     # user_id = cog.get_user(AccessToken=idtoken)['Username']
-
-    user_id = 'asdf-hgd'
-    first = 'fatima'
-    last = 'dantsoho'
-    title = 'web dev'
-    aboutme = 'this is a description'
-    projectID = '1223-4734'
+    body = json.loads(event['body'])
+    idtoken = body['idtoken']
+    cog = boto3.client("cognito-idp", region_name=region)
+    user_id = cog.get_user(AccessToken=idtoken)['Username']
+    first = body['first']
+    last = body['last']
+    title = body['title']
+    aboutme = body['aboutme']
+    projectID = body['ProjectID']
     try:
         data = client.put_item(TableName=tableName,
                                Item={
