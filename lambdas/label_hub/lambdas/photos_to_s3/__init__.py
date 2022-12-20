a188420ca1a1dc921c9fcb896cc30844d2f5fc32
Zehua-Chen <zehua-chen@outlook.com>
2022-12-19 22:49:08 -0500

    [fix] Fix An Issue with photos_to_s3

--- lambdas/label_hub/lambdas/photos_to_s3/__init__.py
+++ lambdas/label_hub/lambdas/photos_to_s3/__init__.py
@@ -1,1 +1,37 @@
-from .lambda_function import lambda_handler as handler
+import sys
+
+sys.path.append("/var/task/vendor")
+
+import os
+import json
+import boto3
+import base64
+from aws_lambda_powertools.utilities.data_classes import event_source, APIGatewayProxyEvent
+
+s3 = boto3.client('s3')
+
+
+@event_source(data_class=APIGatewayProxyEvent)
+def handler(event: APIGatewayProxyEvent, context):
+    assert event.body is not None
+
+    data = json.loads(event.body)
+
+    image = data['file']
+
+    image = image[image.find(",") + 1:]
+    dec = base64.b64decode(image + "===")
+
+    #get userid
+    cog = boto3.client("cognito-idp", region_name='us-east-1')
+    producerid = cog.get_user(AccessToken=data['id_token'])['Username']
+
+    s3.put_object(Bucket=os.environ['s3BucketName'], Key=data['filename'], \
+        Body=dec, Metadata={'labels':data['labels'], 'producerid': producerid})
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'message': 'successful lambda function call'}),
+        'headers': {
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
