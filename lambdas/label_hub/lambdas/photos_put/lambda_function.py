28aa7708aea764a1f5ff8a04689d6ca2e09cf9a5
Zehua-Chen <zehua-chen@outlook.com>
2022-12-19 22:51:31 -0500

    [fix] Fix photos_put

--- lambdas/label_hub/lambdas/photos_put/lambda_function.py
+++ lambdas/label_hub/lambdas/photos_put/lambda_function.py
@@ -8,66 +8,69 @@ import urllib.parse
 import boto3
 from datetime import datetime
 import os
-from aws_lambda_powertools.utilities.data_classes import event_source, APIGatewayProxyEvent
+from aws_lambda_powertools.utilities.data_classes import event_source, S3Event
 
 s3 = boto3.client('s3')
 PRICE = 1
 
 
-@event_source(data_class=APIGatewayProxyEvent)
-def lambda_handler(event: APIGatewayProxyEvent, context):
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'],
-                                    encoding='utf-8')
-    try:
+@event_source(data_class=S3Event)
+def lambda_handler(event: S3Event, context):
+    for record in event.records:
+        bucket = record.s3.bucket.name
 
-        # response = s3.get_object(Bucket=bucket, Key=key)
-        # rekog_labels = detect_labels(key, bucket)
+        key = urllib.parse.unquote_plus(
+            record.s3.get_object.key,
+            encoding='utf-8',)
+        try:
 
-        metadata = s3.head_object(Bucket=bucket, Key=key)
-        producerid = metadata['Metadata']['producerid']
-        labels = metadata['Metadata']['labels']
-        # for label in rekog_labels:
-        #     labels.append(label['Name'])
-        timestamp = metadata['LastModified'].strftime("%Y-%m-%dT%H:%M:%S")
-        json_obj = {
-            "objectKey": key,
-            "bucket": bucket,
-            "createdTimestamp": timestamp,
-            "year": metadata['LastModified'].strftime("%Y"),
-            "month": metadata['LastModified'].strftime("%m"),
-            "day": metadata['LastModified'].strftime("%d"),
-            "producerID": producerid,
-            "price": PRICE,
-            "labels": labels
-        }
+            # response = s3.get_object(Bucket=bucket, Key=key)
+            # rekog_labels = detect_labels(key, bucket)
 
-        # upload the data to opensearch
-        host = os.environ['opensearchEndpointProducer']
-        region = 'us-east-1'
-        credentials = boto3.Session().get_credentials()
-        auth = AWSV4SignerAuth(credentials, region)
-        index_name = 'photos'
-        client = OpenSearch(hosts=[{
-            'host': host,
-            'port': 443
-        }],
-                            http_auth=auth,
-                            use_ssl=True,
-                            verify_certs=True,
-                            connection_class=RequestsHttpConnection)
+            metadata = s3.head_object(Bucket=bucket, Key=key)
+            producerid = metadata['Metadata']['producerid']
+            labels = metadata['Metadata']['labels']
+            # for label in rekog_labels:
+            #     labels.append(label['Name'])
+            timestamp = metadata['LastModified'].strftime("%Y-%m-%dT%H:%M:%S")
+            json_obj = {
+                "objectKey": key,
+                "bucket": bucket,
+                "createdTimestamp": timestamp,
+                "year": metadata['LastModified'].strftime("%Y"),
+                "month": metadata['LastModified'].strftime("%m"),
+                "day": metadata['LastModified'].strftime("%d"),
+                "producerID": producerid,
+                "price": PRICE,
+                "labels": labels
+            }
 
-        response = client.index(index=index_name,
-                                body=json_obj,
-                                id=key,
-                                refresh=True)
-        return {'statusCode': 200, 'body': json.dumps(json_obj)}
-    except Exception as e:
-        print(e)
-        print(
-            'Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'
-            .format(key, bucket))
-        raise e
+            # upload the data to opensearch
+            host = os.environ['opensearchEndpointProducer']
+            region = 'us-east-1'
+            credentials = boto3.Session().get_credentials()
+            auth = AWSV4SignerAuth(credentials, region)
+            index_name = 'photos'
+            client = OpenSearch(hosts=[{
+                'host': host,
+                'port': 443
+            }],
+                                http_auth=auth,
+                                use_ssl=True,
+                                verify_certs=True,
+                                connection_class=RequestsHttpConnection)
+
+            response = client.index(index=index_name,
+                                    body=json_obj,
+                                    id=key,
+                                    refresh=True)
+            return {'statusCode': 200, 'body': json.dumps(json_obj)}
+        except Exception as e:
+            print(e)
+            print(
+                'Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'
+                .format(key, bucket))
+            raise e
 
 
 def detect_labels(photo, bucket):
