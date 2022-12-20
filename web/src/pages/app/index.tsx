a9baeb131891607a45124c7c079dad9e18df0c6e
Zehua-Chen <zehua-chen@outlook.com>
2022-12-20 12:12:32 -0500

    [feat] Ensure User Info Exists

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,9 +1,39 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import { Link } from 'gatsby';
+import * as uuid from 'uuid';
 import HeadContent from 'src/components/HeadContent';
 import Layout from 'src/components/Layout';
+import { useApi } from 'src/services/api/utils';
+import { DefaultApi } from 'src/services/api';
+import { useAuth, Auth } from 'src/services/auth';
+
+async function ensureUserInfoExists(api: DefaultApi, auth: Auth) {
+  try {
+    await api.userinfoGet({ accessToken: auth.accessToken });
+  } catch (e) {
+    await api.userinfoPut({
+      accessToken: auth.accessToken,
+      userInfoPutRequest: {
+        projectID: uuid.v1(),
+        first: '',
+        last: '',
+        title: '',
+        email: '',
+        aboutme: '',
+      },
+    });
+  }
+}
 
 function App() {
+  const api = useApi();
+  const auth = useAuth();
+
+  useEffect(() => {
+    ensureUserInfoExists(api, auth);
+  }, [api, auth]);
+
   return (
     <Layout>
       <ul className='list-group'>
