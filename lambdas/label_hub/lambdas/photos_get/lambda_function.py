213bf8fc128cdc4a97aff812c0fe0f25b5b11ca9
tsai00150 <tsai00150@outlook.com>
2022-12-19 23:15:04 -0500

    update photos-get

--- lambdas/label_hub/lambdas/photos_get/lambda_function.py
+++ lambdas/label_hub/lambdas/photos_get/lambda_function.py
@@ -4,13 +4,20 @@ sys.path.append("/var/task/vendor")
 
 import json
 import boto3
-from opensearch import opensearch
 import urllib
 import os
+import requests
+from requests_aws4auth import AWS4Auth
 from aws_lambda_powertools.utilities.data_classes import event_source, APIGatewayProxyEvent
 
 BUCKET_URL = "https://s3.us-east-1.amazonaws.com"
+region = 'us-east-1'
+service = 'es'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
+
+headers = { "Content-Type": "application/json" }
 
 def build_photo_object(object_key: str, bucket: str, labels: str) -> dict:
     photo = {"url": f"{BUCKET_URL}/{bucket}/{object_key}", "labels": labels}
@@ -36,26 +43,39 @@ def get_images(response: dict):
 
     return {"results": results}
 
-
-def get_tokens(event: dict) -> str:
-    t = event["q"]
-    t = t.strip().lower()
-
-    return t.split(" ")
-
+def query(labels, endpoint):
+    endpoint = 'https://' + endpoint
+    index = 'photos'
+    url = endpoint + '/' + index + '/_search'
+    query = {
+        "size": 25,
+        "query": {
+            "match": {
+                "labels": {
+                    "query"     : ', '.join(labels),
+                    "operator"  : "or",
+                    "fuzziness" : "AUTO"
+                }
+            }
+        }
+    }
+    print(query)
+    
+    r = requests.get(url, auth=awsauth, headers=headers, data=json.dumps(query))
+    print(r.content)
+    response = {
+        "body" : json.loads(r.text)
+    }
+    
+    return response
 
 @event_source(data_class=APIGatewayProxyEvent)
 def lambda_handler(event: APIGatewayProxyEvent, context):
-    print(event)
 
-    labels = get_tokens(event)
-
-    print("Labels")
-    labels = ['cat']
+    labels = [event['queryStringParameters']['labels']]
 
     # Search the photos OpenSearch index for results
-    response = opensearch.query(labels,
-                                os.environ['opensearchEndpointProducer'])
+    response = query(labels, os.environ['opensearchEndpointProducer'])
 
     results = get_images(response)
     print("Results")
