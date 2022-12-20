6e6f4c545a8a7de6da10ed8526d498739b257bef
Zehua-Chen <zehua-chen@outlook.com>
2022-12-19 22:06:08 -0500

    [refactor] Get access token via header

--- lambdas/label_hub/lambdas/userinfo_put/lambda_function.py
+++ lambdas/label_hub/lambdas/userinfo_put/lambda_function.py
@@ -5,49 +5,58 @@ sys.path.append("/var/task/vendor")
 import json
 import boto3
 import os
+from aws_lambda_powertools.utilities.data_classes import event_source, APIGatewayProxyEvent
 
 client = boto3.client('dynamodb')
-tableName = host = os.environ['dynamodb_tableName']
-region = 'us-east-1'
+table_name = host = os.environ['dynamodb_tableName']
+cog = boto3.client("cognito-idp")
 
-def lambda_handler(event, context):
+
+@event_source(data_class=APIGatewayProxyEvent)
+def lambda_handler(event: APIGatewayProxyEvent, context):
     #get userid
     # cog = boto3.client("cognito-idp", region_name=region)
     # user_id = cog.get_user(AccessToken=idtoken)['Username']
-    body = json.loads(event)['header']
-    idtoken = body['idtoken']
-    cog = boto3.client("cognito-idp", region_name=region)
-    user_id = cog.get_user(AccessToken=idtoken)['Username']
+    assert event.body is not None
+
+    body = json.loads(event.body)
+
+    access_token = event.headers["access-token"]
+    user_id = cog.get_user(AccessToken=access_token)['Username']
+
     first = body['first']
     last = body['last']
     title = body['title']
     aboutme = body['aboutme']
     projectID = body['ProjectID']
+
     try:
-        data = client.put_item(TableName=tableName,
-                               Item={
-                                   'id': {
-                                       'S': user_id
-                                   },
-                                   'firstname': {
-                                       'S': first
-                                   },
-                                   'lastname': {
-                                       'S': last
-                                   },
-                                   'title': {
-                                       'S': title
-                                   },
-                                   'email': {
-                                       'S': title
-                                   },
-                                   'aboutme': {
-                                       'S': aboutme
-                                   },
-                                   'projectID':{
-                                        'S': projectID
-                                   }
-                               })
+        data = client.put_item(
+            TableName=table_name,
+            Item={
+                'id': {
+                    'S': user_id
+                },
+                'firstname': {
+                    'S': first
+                },
+                'lastname': {
+                    'S': last
+                },
+                'title': {
+                    'S': title
+                },
+                'email': {
+                    'S': title
+                },
+                'aboutme': {
+                    'S': aboutme
+                },
+                'projectID': {
+                    'S': projectID
+                }
+            },
+        )
 
         response = {
             'statusCode': 200,
